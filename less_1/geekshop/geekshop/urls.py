c600e3d78e2625b1710eb4ae90c5be6bc8bfe2a0
jenkins <pinus@fintech.ru>
2020-08-04 00:18:04 +0300

    Вторая домашняя работа

--- less_1/geekshop/geekshop/urls.py
+++ less_1/geekshop/geekshop/urls.py
@@ -1,18 +1,3 @@
-"""geekshop URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 # from django.contrib import admin
 # from django.urls import path
 
@@ -21,9 +6,14 @@ from django.contrib import admin
 import mainapp.views as mainapp
 
 urlpatterns = [
-    path('', mainapp.main),
-    path('products/', mainapp.products),
-    path('contact/', mainapp.contact),
-    path('admin/', admin.site.urls),
+    path('', mainapp.main, name='main'),
+    path('products/', mainapp.products, name='products'),
+
+    path('products/all/', mainapp.products_all, name='products_all'),
+    path('products/clothes/', mainapp.products_clothes, name='products_clothes'),
+    path('products/another/', mainapp.products_another, name='products_another'),
+
+    path('contact/', mainapp.contact, name='contact'),
+    path('admin/', admin.site.urls, name='admin'),
 ]
 
--- less_1/geekshop/mainapp/views.py
+++ less_1/geekshop/mainapp/views.py
@@ -1,15 +1,55 @@
 from django.shortcuts import render
 
-# Create your views here.
-
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    context = {
+        'title': 'Магазин',
+    }
+    return render(request, 'mainapp/index.html', context)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    # links_menu = {
+    #     {'href': 'products_all', 'name': 'все предложения'},
+    #     {'href': 'products_clothes', 'name': 'Интернет магазины одежды'},
+    #     {'href': 'products_another', 'name': 'Остальные интернет магазины'},
+    # }
+    context = {
+        'title': 'Магазины',
+    #     'links_menu': links_menu
+    }
+    return render(request, 'mainapp/products.html', context)
+
+
+def products_all(request):
+    links_menu = {
+        {'href': 'products_all', 'name': 'все предложения', 'img': ''},
+        {'href': 'products_clothes', 'name': 'Интернет магазины одежды'},
+        {'href': 'products_another', 'name': 'Остальные интернет магазины'},
+    }
+    context = {
+        'title': 'Все Магазины',
+        'links_menu': links_menu
+    }
+    return render(request, 'mainapp/products.html', context)
+
+
+def products_clothes(request):
+    context = {
+        'title': 'Магазины одежды',
+    }
+    return render(request, 'mainapp/products.html', context)
+
+
+def products_another(request):
+    context = {
+        'title': 'Другие магазины',
+    }
+    return render(request, 'mainapp/products.html', context)
 
 
 def contact(request):
-    return render(request, 'mainapp/contact.html')
+    context = {
+        'title': 'Контакты',
+    }
+    return render(request, 'mainapp/contact.html', context)
